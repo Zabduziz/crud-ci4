36e81e7e070c2ffb66f8c439ff383ca6e6ce61b4
Zaidan Abdul Aziz <zaidanabdulaziz03@gmail.com>
2025-03-05 07:15:20 +0700

    add: Fitur pejabat

--- app/Controllers/JabatanController.php
+++ app/Controllers/JabatanController.php
@@ -16,7 +16,8 @@ class JabatanController extends BaseController
     }
     public function index()
     {
-        return view('jabatan/index');
+        $data['jabatan'] = $this->modelJabatan->findAll();
+        return view('jabatan/index', $data);
     }
 
     public function show($id) {
@@ -24,22 +25,37 @@ class JabatanController extends BaseController
     }
 
     public function create() {
-        //
+        return view('jabatan/create');
     }
 
     public function store() {
-        //
+        $data = [
+            'nama_jabatan' => $this->request->getPost('nama_jabatan'),
+            'deskripsi_jabatan' => $this->request->getPost('deskripsi_jabatan')
+        ];
+
+        $this->modelJabatan->save($data);
+        return redirect()->to('jabatan');
     }
 
     public function edit($id) {
-        //
+        $data['jabatan'] = $this->modelJabatan->find($id);
+        return view('jabatan/edit', $data);
     }
 
     public function update($id) {
-        //
+        $data = [
+            'id' => $id,
+            'nama_jabatan' => $this->request->getPost('nama_jabatan'),
+            'deskripsi_jabatan' => $this->request->getPost('deskripsi_jabatan')
+        ];
+
+        $this->modelJabatan->save($data);
+        return redirect()->to('jabatan');
     }
 
     public function delete($id) {
-        //
+        $this->modelJabatan->delete($id);
+        return redirect()->to('jabatan');
     }
 }
--- app/Views/jabatan/index.php
+++ app/Views/jabatan/index.php
@@ -4,14 +4,34 @@
 <div class="my-3 p-3 bg-body rounded shadow-sm">
     <div class="d-flex justify-content-between border-bottom py-2">
         <h3 class="pb-2 mb-0">Data Jabatan</h3>
+        <a href="/jabatan/create" class="btn btn-primary">Tambah Data</a>
     </div>
     <div class="pt-3">
         <table class="table table-bordered table-stripep">
-            <tr>
-                <th>No.</th>
-                <th>Nama Jabatan</th>
-                <th>Deskripsi</th>
-            </tr>
+            <thead>
+                <tr>
+                    <th>No.</th>
+                    <th>Nama Jabatan</th>
+                    <th>Deskripsi</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($jabatan as $key => $row) { ?>
+                    <tr>
+                        <td><?= $key+1 ;?></td>
+                        <td><?= $row->nama_jabatan ;?></td>
+                        <td><?= $row->deskripsi_jabatan ;?></td>
+                        <td>
+                            <form action="/jabatan/delete/<?= $row->id; ?>" method="post">
+                                <a href="/jabatan/edit/<?= $row->id; ?>" class="btn btn-warning">Edit</a>
+                                <?= csrf_field() ;?>
+                                <button type="submit" class="btn btn-danger">Hapus</button>
+                            </form>
+                        </td>
+                    </tr>
+                <?php } ;?>
+            </tbody>
         </table>
     </div>
 </div>
